8515ff55ab303930d404055b60e44a4190857de8
Endrio Alex Korb <alexkorb95@gmail.com>
2023-05-22 18:35:22 -0300

    Python

--- informacao_salas.py
+++ informacao_salas.py
@@ -1,37 +1,45 @@
 
 import os
 import csv
+
 from docx import Document
-import cv2
+from docx.enum.style import WD_STYLE_TYPE
+from docx.enum.table import WD_TABLE_ALIGNMENT
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.shared import Pt
+from docx.shared import RGBColor
+from docx.shared import Inches
+
+
 
 
 if __name__ == "__main__":
 
-    documento = Document("modelo_informacao.docx")
+    document = Document()   
 
-
+    # Estilos da informações
+    styles = document.styles
+    p = styles.add_style("paragrafo", WD_STYLE_TYPE.PARAGRAPH) 
+    p.font.name = "Calibri Light (Títulos)"
+    p.font.size = Pt(45)
+    p.font.bold = True
 
     with open(os.path.join(os.getcwd(), "Organização das salas.csv"), "r") as arquivo:
         arquivo_csv = csv.reader(arquivo, delimiter = ";")
 
         for linha in arquivo_csv:
 
-            sala = str(linha[1])
-            projetor = str(linha[2])
-            patrimonio = str(linha[3])
-            capacidade = str(linha[4])
+            sala = "SALA " + str(linha[1])
+            projetor = "PROJETOR " + str(linha[2])
+            patrimonio = "PATRIMÔNIO " + str(linha[3])
+            capacidade = "CAPACIDADE " +str(linha[4])
 
-            for paragrafo in documento.paragraphs:
+            document.add_paragraph(sala, style="paragrafo")
+            document.add_paragraph(projetor,style="paragrafo")
+            document.add_paragraph(patrimonio, style="paragrafo")
+            document.add_paragraph(capacidade, style="paragrafo")
+            document.add_picture("logo.png", width=Inches(6.5))
 
-                referencia = {
-                "sl" : sala ,
-                "pj" : projetor,
-                "pt" : patrimonio,
-                "cp" : capacidade,
-            }
-                for paragrafo in documento.paragraphs:
-                    for codigo in referencia:
-                        valor = referencia[codigo]
-                        paragrafo.text = paragrafo.text.replace(codigo, valor)
 
-                documento.save(f"Sala - {sala}.docx")
+
+            document.save(f"{sala}.docx")
